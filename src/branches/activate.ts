211ef983ec3b521ea4b71afc97fa0d5a50bdd36e
lamartinecabral <lamartine.cb@gmail.com>
2026-05-29 13:53:22 -0300

    refactor: remove unused description handling and delete obsolete description files

--- src/branches/activate.ts
+++ src/branches/activate.ts
@@ -12,10 +12,6 @@ if (!branch || !fs.existsSync(`src/branches/${branch}`)) {
   throw new Error(`Valid branches are: ${branches.join(", ")}`);
 }
 
-const description: string = JSON.parse(
-  fs.readFileSync(`src/branches/${branch}/description.json`).toString(),
-);
-
 const setPkgVersion = (pkg: string) => {
   const obj: Pkg = JSON.parse(pkg);
   const currentBranch = obj.version.match(/-(.*)/)?.[1];
@@ -23,10 +19,5 @@ const setPkgVersion = (pkg: string) => {
   return pkg.replace(obj.version, obj.version.replace(currentBranch, branch));
 };
 
-const setPkgDescription = (pkg: string) => {
-  const obj: Pkg = JSON.parse(pkg);
-  return pkg.replace(obj.description, description);
-};
-
 const pkg = fs.readFileSync("package.json").toString();
-fs.writeFileSync("package.json", setPkgVersion(setPkgDescription(pkg)));
+fs.writeFileSync("package.json", setPkgVersion(pkg));
